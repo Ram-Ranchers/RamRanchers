22f3199d23173d0797156aa317999261b4e4ee87
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-02-11 14:49:35 +0000

    Started implementing a behaviour tree for the ram

---
+++ Assets/Scripts/DecisionMakingAI/GuardBT.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace DecisionMakingAI
+{
+    public class GuardBT
+    {
+        //public Transform[] waypoints;
+
+        //public static float speed = 2f;
+
+        //protected override Node SetupTree()
+        //{
+        //}
+    }
+}
