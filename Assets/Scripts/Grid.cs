21ca7202b90659ea07053fe0870ada341c974927
Sean Nichols <78358189+OGreatHippo@users.noreply.github.com>
2022-02-11 14:48:48 +0000

    did stuff

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    private Renderer lineRenderer;
+
     private float gridSizeX;
     private float gridSizeZ;
+
+    [SerializeField] private float size = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        gridSizeX = gameObject.bounds.size.x;
-        gridSizeZ = gameObject.bounds.size.z;
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+
+        gridSizeX = gameObject.transform.localScale.x / size;
+        gridSizeZ = gameObject.transform.localScale.z / size;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < gridSizeX; i++)
+        {
+            for (int j = 0; j < gridSizeZ; j++)
+            {
+                Vector3 pos0 = new Vector3(mSquare[i, j].getX(), zOffset, mSquare[i, j].getY());
+                Vector3 pos1 = new Vector3(mSquare[i, j].getXPlus(), zOffset, mSquare[i, j].getY());
+                Vector3 pos2 = new Vector3(mSquare[i, j].getXPlus(), zOffset, mSquare[i, j].getYPlus());
+                Vector3 pos3 = new Vector3(mSquare[i, j].getX(), zOffset, mSquare[i, j].getYPlus());
+
+                lineRenderer.SetWidth(10, 10);
+                lineRenderer.SetColors(Color.red, Color.red);
+
+                lineRenderer.SetPosition(0, pos0);
+                lineRenderer.SetPosition(1, pos1);
+                lineRenderer.SetPosition(2, pos2);
+                lineRenderer.SetPosition(3, pos3);
+                lineRenderer.SetPosition(4, pos0);
+            }
+        }
     }
-}
+}   
