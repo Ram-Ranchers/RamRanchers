f2732448f1abb9a6ace955f8023cc513957c684f
Sean Nichols <78358189+OGreatHippo@users.noreply.github.com>
2022-02-25 14:43:05 +0000

    started to plagurise

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,33 +4,58 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    [SerializeField] private float gridWidth = 1f;
-    [SerializeField] private float gridLength = 1f;
-    [SerializeField] private float gridHeight = 1f;
-    [SerializeField] private int scale = 2;
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
 
-    private Vector3 cells;
-
-    // Start is called before the first frame update
-    void Start()
+    public Grid(int width, int height, float cellSize)
     {
-        for(int i = 0; i < gridWidth; i++)
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for(int i = 0; i < gridArray.GetLength(0); i++)
         {
-            for(int j = 0; j < gridLength; j++)
+            for(int j = 0; j < gridArray.GetLength(1); j++)
             {
-                for(int k = 0; k < gridHeight; k++)
-                {
-                    cells.x = i * scale;
-                    cells.y = k * scale;
-                    cells.z = j * scale;
-                }
+                Debug.Log(i + ", " + j);
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // https://www.youtube.com/watch?v=waEsGu--9P8&t=0s
+        //[SerializeField] private float gridWidth = 1f;
+        //[SerializeField] private float gridLength = 1f;
+        //[SerializeField] private float gridHeight = 1f;
+        //[SerializeField] private int scale = 2;
+
+    //private List<Vector3> cells;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    for(int i = 0; i < gridWidth; i++)
+    //    {
+    //        for(int j = 0; j < gridLength; j++)
+    //        {
+    //            for(int k = 0; k < gridHeight; k++)
+    //            {
+    //                cells.x = i * scale;
+    //                cells.y = k * scale;
+    //                cells.z = j * scale;
+
+    //                print(cells);
+    //            }
+    //        }
+    //    }
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
 }
