8aeeb45558d4e9b13ce9e5c685eddd8d1e1483a3
Sean Nichols <w9107026@live.tees.ac.uk>
2022-02-28 18:18:27 +0000

    removed unnecessary commented out code

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -33,36 +33,5 @@ public class Grid : MonoBehaviour
         }
     }
 
-    // https://www.youtube.com/watch?v=waEsGu--9P8&t=0s
-    //[SerializeField] private float gridWidth = 1f;
-    //[SerializeField] private float gridLength = 1f;
-    //[SerializeField] private float gridHeight = 1f;
-    //[SerializeField] private int scale = 2;
-
-    //private List<Vector3> cells;
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    for(int i = 0; i < gridWidth; i++)
-    //    {
-    //        for(int j = 0; j < gridLength; j++)
-    //        {
-    //            for(int k = 0; k < gridHeight; k++)
-    //            {
-    //                cells.x = i * scale;
-    //                cells.y = k * scale;
-    //                cells.z = j * scale;
-
-    //                print(cells);
-    //            }
-    //        }
-    //    }
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
+    
 }
