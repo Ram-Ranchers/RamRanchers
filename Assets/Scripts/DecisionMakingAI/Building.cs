af4c6a435477d936699d33653e42a20bd7104f22
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-03-02 11:13:55 +0000

    Can place buildings down

--- Assets/Scripts/DecisionMakingAI/Building.cs
+++ Assets/Scripts/DecisionMakingAI/Building.cs
@@ -1,22 +1,100 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace DecisionMakingAI
 {
+    public enum BuildingPlacement
+    {
+        Valid,
+        Invalid,
+        Fixed
+    }
+    
     public class Building
     {
         private BuildingData _date;
         private Transform _transform;
         private int _currentHealth;
-
+        private BuildingPlacement _placement;
+        private List<Material> _materials;
+        private BuildingManager _buildingManager;
+        
         public Building(BuildingData data)
         {
             _date = data;
             _currentHealth = data.HP;
-            
-            GameObject g = GameObject.Instantiate(Resources.Load($"Prefabs/RTS/{_date.Code}")) as GameObject;
+
+            GameObject g = GameObject.Instantiate(Resources.Load($"Prefabs/Buildings/{_date.Code}")) as GameObject;
             _transform = g.transform;
+            
+            _materials = new List<Material>();
+            foreach (Material material in _transform.Find("Mesh").GetComponent<Renderer>().materials)
+            {
+                _materials.Add(new Material(material));
+            }
+            
+            _buildingManager = g.GetComponent<BuildingManager>();
+            _placement = BuildingPlacement.Valid;
+            SetMaterials();
         }
 
+        public void SetMaterials()
+        {
+            SetMaterials(_placement);
+        }
+
+        public void SetMaterials(BuildingPlacement placement)
+        {
+            List<Material> materials;
+            if (placement == BuildingPlacement.Valid)
+            {
+                Material refMaterial = Resources.Load("Materials/Valid") as Material;
+                materials = new List<Material>();
+                for (int i = 0; i < _materials.Count; i++)
+                {
+                    materials.Add(refMaterial);
+                }
+            }
+            else if (placement == BuildingPlacement.Invalid)
+            {
+                Material refMaterial = Resources.Load("Materials/Invalid") as Material;
+                materials = new List<Material>();
+                for (int i = 0; i < _materials.Count; i++)
+                {
+                    materials.Add(refMaterial);
+                }
+            }
+            else if (placement == BuildingPlacement.Fixed)
+            {
+                materials = _materials;
+            }
+            else
+            {
+                return;
+            }
+
+            _transform.Find("Mesh").GetComponent<Renderer>().materials = materials.ToArray();
+        }
+        
+        public void Place()
+        {
+            _placement = BuildingPlacement.Fixed;
+
+            SetMaterials();
+            
+            _transform.GetComponent<BoxCollider>().isTrigger = false;
+        }
+
+        public void CheckValidPlacement()
+        {
+            if (_placement == BuildingPlacement.Fixed)
+            {
+                return;
+            }
+
+            _placement = _buildingManager.CheckPlacement() ? BuildingPlacement.Valid : BuildingPlacement.Invalid;
+        }
+        
         public void SetPosition(Vector3 position)
         {
             _transform.position = position;
@@ -26,7 +104,8 @@ namespace DecisionMakingAI
         public Transform Transform => _transform;
         public int HP { get => _currentHealth; set => _currentHealth = value; }
         public int MaxHp => _date.HP;
-
+        public bool IsFixed => _placement == BuildingPlacement.Fixed;
+        public bool HasValidPlacement => _placement == BuildingPlacement.Valid;
         public int DataIndex
         {
             get
--- Assets/Scripts/DecisionMakingAI/BuildingPlacer.cs
+++ Assets/Scripts/DecisionMakingAI/BuildingPlacer.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace DecisionMakingAI
@@ -25,21 +24,46 @@ namespace DecisionMakingAI
                     CancelPlaceBuilding();
                     return;
                 }
+
+                _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(_ray, out _raycastHit, 1000f, Globals.Terrain_Layer_Mask))
+                {
+                    _placedBuilding.SetPosition(_raycastHit.point);
+                    if (_lastPlacementPosition != _raycastHit.point)
+                    {
+                        _placedBuilding.CheckValidPlacement();
+                    }
+
+                    _lastPlacementPosition = _raycastHit.point;
+                }
+
+                if (_placedBuilding.HasValidPlacement && Input.GetMouseButtonDown(0))
+                {
+                    PlaceBuilding();
+                }
             }
         }
 
         void PreparePlacedBuilding(int buildingDataIndex)
         {
-            if (_placedBuilding !=  null)
+            if (_placedBuilding !=  null && !_placedBuilding.IsFixed)
             {
                 Destroy(_placedBuilding.Transform.gameObject);
             }
 
             Building building = new Building(Globals.Building_Data[buildingDataIndex]);
+            building.Transform.GetComponent<BuildingManager>().Initialise(building);
             _placedBuilding = building;
             _lastPlacementPosition = Vector3.zero;
         }
 
+        void PlaceBuilding()
+        {
+            _placedBuilding.Place();
+            
+            PreparePlacedBuilding(_placedBuilding.DataIndex);
+        }
+        
         void CancelPlaceBuilding()
         {
             Destroy(_placedBuilding.Transform.gameObject);
