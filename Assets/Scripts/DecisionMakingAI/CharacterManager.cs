1dfb0fad4d68cfeffe76b6543cb29a99c45a9ce7
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2022-03-07 10:13:23 +0000

    Working on unit movement

--- Assets/Scripts/DecisionMakingAI/CharacterManager.cs
+++ Assets/Scripts/DecisionMakingAI/CharacterManager.cs
@@ -1,13 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+
 namespace DecisionMakingAI
 {
     public class CharacterManager : UnitManager
     {
+        public NavMeshAgent agent;
+        
         private Character _character;
 
         public override Unit Unit
         {
             get { return _character; }
             set { _character = value is Character ? (Character)value : null; }
+        }
+
+        public void MoveTo(Vector3 targetPosition)
+        {
+            agent.destination = targetPosition;
         }
     }
 }
--- Assets/Scripts/DecisionMakingAI/GameManager.cs
+++ Assets/Scripts/DecisionMakingAI/GameManager.cs
@@ -5,11 +5,37 @@ namespace DecisionMakingAI
 {
     public class GameManager : MonoBehaviour
     {
+        private Ray _ray;
+        private RaycastHit _raycastHit;
+        
         private void Awake()
         {
             DataHandler.LoadGameData();
         }
 
+        private void Update()
+        {
+            CheckUnitsNavigation();
+        }
+
+        private void CheckUnitsNavigation()
+        {
+            if (Globals.Selected_Units.Count > 0 && Input.GetMouseButtonUp(1))
+            {
+                _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(_ray, out _raycastHit, 1000f, Globals.Terrain_Layer_Mask))
+                {
+                    foreach (UnitManager um in Globals.Selected_Units)
+                    {
+                        if (um.GetType() == typeof(CharacterManager))
+                        {
+                            ((CharacterManager)um).MoveTo(_raycastHit.point);
+                        }
+                    }
+                }
+            }
+        }
+        
         //public float producingRate = 3f;
 
        //public void Start()
